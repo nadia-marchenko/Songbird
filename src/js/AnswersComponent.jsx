0599f8ddda32051b034c11a484feffb4efb98b10
Nadya Marchanka <nadia.besarab@gmail.com>
2020-08-13 21:52:48 +0300

    feat: add markdown right answer

--- src/js/AnswersComponent.jsx
+++ src/js/AnswersComponent.jsx
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import ReactAudioPlayer from 'react-audio-player';
+
+import Image from 'react-bootstrap/Image';
+import image2 from '../assets/images/default-bird.jpeg';
+import song from '../assets/images/song.mp3';
 
 class AnswersComponent extends Component {
   constructor() {
@@ -45,10 +50,27 @@ class AnswersComponent extends Component {
                   </ul>
                 </div>
               </Col>
-              <Col sm = {6}>
-                <div className="raised-block bg-dark rounded">
-                  <h3>Tweets</h3>
-                </div>
+              <Col sm = {6} className="raised-block bg-dark rounded">
+                <Row>
+                  <Col sm = {12} md = {5}>
+                    <Image className='default-answer-img' src= {image2} width='200px' alt="bird" rounded fluid/>
+                  </Col>
+                  <Col sm = {12} md = {7}>
+                    <h4 className='bird-name'>Пеликан</h4>
+                    <hr/>
+                    <h6 className='translate-bird-name'>Pelikan</h6>
+                    <hr/>
+                    <ReactAudioPlayer
+                      className = 'audio-player player-answer'
+                      src = {song}
+                      controls
+                    />
+                  </Col>
+                </Row>
+                <Row>
+                  <p className='birds-description'>Ворон – крупная птица. Длина тела достигает 70 сантиметров, размах крыльев – до полутора метров. Вороны населяют окрестности Тауэра. В Англии бытует поверье, что в день, когда черные вороны улетят от Тауэра, монархия рухнет.</p>
+                </Row>
+                
               </Col>
             </Row>
           </Container>
