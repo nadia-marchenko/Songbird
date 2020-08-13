85734bfc25654fc05151353acee5e345c3e32b51
Nadya Marchanka <nadia.besarab@gmail.com>
2020-08-13 17:16:14 +0300

    feat: add markdown to answer's options

--- src/js/AnswersComponent.jsx
+++ src/js/AnswersComponent.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 class AnswersComponent extends Component {
   constructor() {
@@ -9,7 +12,47 @@ class AnswersComponent extends Component {
   render() {
     return (
       <>
-        <h1 className = 'logo'>{this.name}</h1>
+        <div className = 'wrapper answers-area'>
+          <Container>
+            <Row>
+              <Col sm = {6}>
+                <div className="raised-block bg-dark rounded">
+                  <ul className='item-list list-group'>
+                    <li className='list-group-item'>
+                      <span className="control-label"/>
+                      Ворон
+                    </li>
+                    <li className='list-group-item'>
+                      <span className="control-label"/>
+                      Синица
+                    </li>
+                    <li className='list-group-item'>
+                      <span className="control-label"/>
+                      Воробей
+                    </li>
+                    <li className='list-group-item'>
+                      <span className="control-label correct-label"/>
+                      Кукушка
+                    </li>
+                    <li className='list-group-item'>
+                      <span className="control-label error-label"/>
+                      Ласточка
+                    </li>
+                    <li className='list-group-item'>
+                      <span className="control-label"/>
+                      Аист
+                    </li>
+                  </ul>
+                </div>
+              </Col>
+              <Col sm = {6}>
+                <div className="raised-block bg-dark rounded">
+                  <h3>Tweets</h3>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </div>
       </>
     );
   };
